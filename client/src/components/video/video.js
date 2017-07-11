ff7869a0aa3d603a2adb741cf1943b7f584e3c58
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-07-11 13:35:55 -0400

    end of module11 - video components

---
+++ client/src/components/video/video.js
@@ -0,0 +1,55 @@
+// This file will serve as our parent component
+
+// Import the neccessary modules
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search-bar';
+import VideoDetail from './video-detail';
+
+
+const API_KEY = "AIzaSyALdgt9CRy5OXu_ndRTSMTwH3L2-C928lg";
+
+
+// Create a Video component
+class Video extends Component {
+
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch("bucketlist");
+	};
+
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (video) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	};
+
+
+	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+			</div>
+		);
+	};
+
+
+};
+
+
+
+
+export default Video;

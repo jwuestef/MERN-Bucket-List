cffa9357886cde5dde703a3879590e7099c709f9
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-07-11 12:53:51 -0400

    end of module11.1 - NavbarHeader

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 
 var createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -17,3 +19,11 @@ ReactDOM.render(
 	, document.querySelector('.container')
 );
 
+
+// The Provider is what communicates with connected components. 
+<Provider store={createStoreWithMiddleware(reducers)}>
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+		</Route>
+	</Router>
+</Provider>

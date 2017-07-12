c6b6921f2488c677782205ecb77930b7576bc323
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-07-12 14:47:15 -0400

    end of module16 - auth state

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,10 @@
 // Axios is a promise based library that allows us to make AJAX requests
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER, UNAUTH_USER } from './types';
+import authReducer from '../reducers/auth_reducer';
+
+
 
 export const CREATE_POSTS = 'CREATE_POSTS';
 
@@ -24,10 +29,16 @@ export function signinUser({email, password}) {
 	return function(dispatch) {
 		axios.post(`${ROOT_URL}/signin`, {email, password})
 			.then(response => {
+				// This only kickstarts if the request was good...
+				// We now update the state to indicate authenticated user
+				dispatch({type: AUTH_USER});
+				// This will put the token in localStorage. It's safe!
+				localStorage.setItem('token', response.data.token);
+				// This sends us off to the /newitem view
 				browserHistory.push('/newitem');
 			})
 			.catch( () => {
-				
+
 			})
 	};
 

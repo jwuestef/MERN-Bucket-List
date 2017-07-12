75aa3f48ab8a555c3d7ce98764a223eaedf9f89d
Jeremy Wuestefeld <jwuestef@gmail.com>
2017-07-12 10:14:31 -0400

    end of module-12.3-newPostForm

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/app';
 import reducers from './reducers';
 import Signin from './components/auth/signin';
+import ListItem from './components/list/new-list-item';
 
 
 var createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -18,6 +19,7 @@ ReactDOM.render(
 		<Router history={browserHistory}>
 			<Route path="/" component={App}>
 				<Route path="signin" component={Signin} />
+				<Route path="newitem" component={ListItem} />
 			</Route>
 		</Router>
 	</Provider>
